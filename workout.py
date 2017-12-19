b178f545e7f91a50bc811f982171c38a76381227
Greg Jones <gtjones1@aggies.ncat.edu>
2017-12-18 22:41:35 -0600

    added bootstrap component, and fixed the form rendeing bug

--- workout.py
+++ workout.py
@@ -3,22 +3,24 @@ from model import db, User
 import bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'DontTellAnyone!'
-
+"""
+flask_bcrypt = Bcrypt(app)
+"""
 @app.route('/')
 def index():
     return render_template('index.html')
 
 app.route('/login', methods=['POST'])
 def login():
-     user = User.query.filter_by(name=request.form['username'].encode('utf-8')).first()
+    user = User.query.filter_by(name=request.form['username'].encode('utf-8')).first()
 
     if user is not None:
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), user.password.encode('utf-8')) == user.password.encode('utf-8'):
             session['username'] = request.form['username']
-            return redirect url_for('index')
-
-        return 'Password is incorrect'
-
+            return url_for('index')
+        return 'password is incorrect' 
+    
+    
     return 'User does not exist!'
 
 
